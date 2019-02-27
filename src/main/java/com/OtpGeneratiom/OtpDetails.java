aa4c64033d6615e54417e317c1b353562a8d31f4
Shubham <spandey@tcd.ie>
2019-02-27 15:40:41 +0000

    Initial commit

---
+++ src/main/java/com/OtpGeneratiom/OtpDetails.java
@@ -0,0 +1,47 @@
+package com.OtpGeneratiom;
+
+public class OtpDetails {
+	private long id;
+	private int otp;
+	private String phoneNumber;
+	private String message;
+	private String response;
+	public String getResponse() {
+		return response;
+	}
+	public void setResponse(String response) {
+		this.response = response;
+	}
+	public OtpDetails(long id, int otp, String phoneNumber, String message,String response) {
+		super();
+		this.id = id;
+		this.otp = otp;
+		this.phoneNumber = phoneNumber;
+		this.message = message;
+		this.response=response;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public int getOtp() {
+		return otp;
+	}
+	public void setOtp(int otp) {
+		this.otp = otp;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
---
+++ src/main/java/com/OtpGeneratiom/SendSms.java
@@ -0,0 +1,41 @@
+package com.OtpGeneratiom;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+public class SendSms {
+	public String sendSms(String sms,String phoneNumber) {
+		try {
+			// Construct data
+			String apiKey = "apikey=" + "abwHS/qxlRI-OwnlLjeDAIaFCBBWCxkdIXa1OV82tU";
+			String message = "&message=" +sms;
+			String sender = "&sender=" + "Fantasy Trip";
+			String numbers = "&numbers=" + phoneNumber;
+
+			// Send data
+			HttpURLConnection conn = (HttpURLConnection) new URL("https://api.txtlocal.com/send/?").openConnection();
+			String data = apiKey + numbers + message + sender;
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
+			conn.getOutputStream().write(data.getBytes("UTF-8"));
+			final BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			final StringBuffer stringBuffer = new StringBuffer();
+			String line;
+			while ((line = rd.readLine()) != null) {
+				stringBuffer.append(line);
+			}
+			rd.close();
+
+			return stringBuffer.toString();
+		} catch (Exception e) {
+			System.out.println("Error SMS "+e);
+			return "Error "+e;
+		}
+	}
+}
